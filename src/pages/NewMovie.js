0a2b712f917dca67b31043d9bfa125bb55693cbc
Lucas Werner <lucas.1werner@gmail.com>
2020-07-13 02:22:33 -0300

    subindo projeto

---
+++ src/pages/NewMovie.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
+
+class NewMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { shouldRedirect: false };
+  }
+
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  render() {
+    const { shouldRedirect } = this.state;
+    const { history } = this.props;
+    if (shouldRedirect) {
+      history.push('/');
+    }
+    return (
+      <div data-testid="new-movie">
+        <MovieForm onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+NewMovie.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default NewMovie;
