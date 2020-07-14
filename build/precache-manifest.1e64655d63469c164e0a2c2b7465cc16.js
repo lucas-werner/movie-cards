0f6e14471356fb1026c0f1e727ce0fe7cc8cad4b
Lucas Werner <lucas.1werner@gmail.com>
2020-07-14 00:05:16 -0300

    ok

---
+++ build/precache-manifest.1e64655d63469c164e0a2c2b7465cc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13729f8af61c24c31e4593866521759",
+    "url": "/movie-cards/index.html"
+  },
+  {
+    "revision": "f0e2d657dfbce086c72a",
+    "url": "/movie-cards/static/css/main.e26099c4.chunk.css"
+  },
+  {
+    "revision": "ee477802c74331ec8048",
+    "url": "/movie-cards/static/js/2.4e172b4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-cards/static/js/2.4e172b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e2d657dfbce086c72a",
+    "url": "/movie-cards/static/js/main.ae052466.chunk.js"
+  },
+  {
+    "revision": "41792197fc5600d7dd8e",
+    "url": "/movie-cards/static/js/runtime-main.2b784465.js"
+  }
+]);
